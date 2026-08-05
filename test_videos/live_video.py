571c38568782c59317eea7ede1e43977f19a9f10
NUHA <nuhieee@gmail.com>
2026-08-05 17:37:56 +0530

    Generated view block algorithm

--- test_videos/live_video.py
+++ test_videos/live_video.py
@@ -1,10 +1,11 @@
 import cv2
+from detection.viewblock import detect_viewblock
 
-ip='172.16.11.156'
-user='camera3'
-password='Camera@156'
+ip='172.16.10.235'
+user='camera2'
+password='Camera@235'
 
-url=f"rtsp://{'camera3'}:{'Camera@156'}@{'172.16.11.156'}:554/cam/realmonitor?channel=1&subtype=0"
+url=f"rtsp://{'camera2'}:{'Camera@235'}@{'172.16.10.235'}:554/cam/realmonitor?channel=1&subtype=0"
 
 cap=cv2.VideoCapture(url)
 
@@ -18,6 +19,11 @@ while True:
     if not ret:
         print('Failed Live Feed')
         break
+    blocked,ratio=detect_viewblock(frame)
+    print(blocked)
+    print(ratio)
+    if blocked:
+        cv2.putText(frame,"CAMERA VIEW BLOCKED",(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),3)
     cv2.imshow("CamShield Live Feed",frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
